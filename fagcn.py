b448d09ced48e36e1353242ad42cf81253e97d3a
Claudio_Casellato <claudiocasellatoml@gmail.com>
2022-06-16 01:33:39 +0200

    FAGCN model

---
+++ fagcn.py
@@ -0,0 +1,126 @@
+from gatv2 import GATv2Model
+import tensorflow as tf
+from tensorflow.keras import backend as k
+from tensorflow.keras import layers as l
+from tensorflow.keras import models as m
+from tensorflow.keras import initializers as init
+from tensorflow.keras import regularizers as reg
+from tensorflow.keras import activations as act
+import numpy as np
+from spektral.layers.ops import unsorted_segment_softmax
+from spektral.layers.convolutional import GATConv
+import datetime
+
+
+def sparse_diag(A: tf.sparse.SparseTensor):
+    diag = tf.sparse.reduce_sum(A, -1)
+    i = tf.range(A.shape[-1], dtype=tf.int64)[:, None]
+    ii = tf.concat([i, i], -1)
+    return tf.sparse.SparseTensor(ii, diag, A.shape)
+
+
+class FAGCNLayer(l.Layer):
+    def __init__(self, dropout, **kwargs):
+        super(FAGCNLayer, self).__init__(**kwargs)
+        self.dropout = l.Dropout(dropout)
+
+    def build(self, input_shape):
+        x, a = input_shape
+        self.kernel = self.add_weight(shape=(2 * x[-1],1), name="kernel", initializer="glorot_normal")
+
+    def call(self, inputs, *args, **kwargs):
+        x, a = inputs
+        N = tf.shape(x, out_type=a.indices.dtype)[-2]
+        diag = sparse_diag(a)
+        sqrt_diag = tf.math.sqrt(diag.values)[:, None]  # Nx1
+        i, j = a.indices[:, 0], a.indices[:, 1]
+        x_i = tf.gather(x, i)  # ExF
+        x_j = tf.gather(x, j)  # ExF
+        x_ij = tf.concat([x_i, x_j], -1)  # Ex2F
+        alpha = tf.nn.tanh(x_ij @ self.kernel)  # Ex1
+        di = tf.gather(sqrt_diag, i)  # Ex1
+        dj = tf.gather(sqrt_diag, j)  # Ex1
+        dij = tf.where(di * dj == 0.0, k.epsilon(), di * dj)  # Ex1
+        g_coeff = alpha / dij  # Ex1
+        g_coeff = self.dropout(g_coeff)
+        x_i_weighted = x_i * g_coeff  # ExF
+        x_i_summed = tf.math.unsorted_segment_sum(x_i_weighted, j, N)  # NxF
+        #x_j_prime = self.epsilon * x + x_i_summed  # NxF
+        return x_i_summed
+
+
+class FAGCNModel(m.Model):
+    def __init__(self, hidden_sizes, n_layers, dropout, activation="elu", epsylon=0.2, **kwargs):
+        super(FAGCNModel, self).__init__(**kwargs)
+        self.hidden_sizes = hidden_sizes
+        assert len(hidden_sizes) == 2
+        self.n_layers = n_layers
+        self.activation = activation
+        self.epsylon = epsylon
+        self.dropout = l.Dropout(dropout)
+        self.graph_layers = [FAGCNLayer(dropout) for _ in range(n_layers)]
+        self.dense_layers = [l.Dense(h, activation) for h in hidden_sizes]
+
+    def call(self, inputs, training=None, mask=None):
+        x, a = inputs
+        x = self.dense_layers[0](x)
+        self.dropout(x)
+        raw = x
+        for g in self.graph_layers:
+            x_prime = g([x, a])
+            x = self.epsylon * raw + x_prime
+        x = self.dense_layers[1](x)
+        return x
+
+
+if __name__ == "__main__":
+    from spektral import datasets
+    import matplotlib.pyplot as plt
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sparse", action="store_true")
+    parser.add_argument("--epochs", type=int)
+    args = parser.parse_args()
+    sparse = args.sparse
+    epochs = args.epochs
+
+    data = datasets.Citation("cora")
+    data.download()
+    a = data.graphs[0].a.todense()
+    x = data.graphs[0].x
+    y = data.graphs[0].y
+    if sparse:
+        a = tf.sparse.from_dense(a)
+    else:
+        N = 500
+        a = a[None, :N, :N]
+        x = x[None, :N]
+        y = y[None, :N]
+
+    fgcn = FAGCNModel(hidden_sizes=[25, 7], n_layers=4, dropout=0.2)
+    gat2 = GATv2Model(channels=[25, 15, 7], hidden_sizes=[25, 15, 7], heads=[1, 1, 1])
+    loss_hist, loss2_hist = [], []
+    optimizer = tf.keras.optimizers.Adam()
+    optimizer2 = tf.keras.optimizers.Adam()
+    for i in range(400):
+        print(f"Epoch: {i}")
+        with tf.GradientTape() as tape:
+            out = fgcn([x, a])
+            loss = tf.keras.losses.categorical_crossentropy(y, out, from_logits=True)
+            loss = tf.reduce_mean(loss)
+            loss_hist.append(loss)
+        grads = tape.gradient(loss, fgcn.trainable_weights)
+        optimizer.apply_gradients(zip(grads, fgcn.trainable_weights))
+        with tf.GradientTape() as tape:
+            out2 = gat2([x, a])
+            loss2 = tf.keras.losses.categorical_crossentropy(y, out2, from_logits=True)
+            loss2 = tf.reduce_mean(loss2)
+            loss2_hist.append(loss2)
+        grads2 = tape.gradient(loss2, gat2.trainable_weights)
+        optimizer2.apply_gradients(zip(grads2, gat2.trainable_weights))
+
+    plt.plot(loss_hist, label="FGCN Loss")
+    plt.plot(loss2_hist, label="GATv2 Loss")
+    plt.legend()
+    plt.show()
--- gatv2.py
+++ gatv2.py
@@ -8,12 +8,14 @@ from tensorflow.keras import activations as act
 import numpy as np
 from spektral.layers.ops import unsorted_segment_softmax
 from spektral.layers.convolutional import GATConv
+import datetime
 
 
 class GATv2Layer(l.Layer):
     def __init__(self, heads, channels,
                  concatenate_output=False,
                  activation="relu",
+                 dropout=0.2,
                  residual=False,
                  initializer=init.glorot_normal,
                  regularizer=None,
@@ -28,25 +30,26 @@ class GATv2Layer(l.Layer):
         self.initializer = initializer
         self.regularizer = regularizer
         self.return_attention = return_attention
+        self.dropout = l.Dropout(dropout)
 
     def build(self, input_shape):
         x, a = input_shape
-        w_shape = (self.heads, x[-1], self.channels)
-        a_shape = (self.heads, self.channels)
+        self.w_shape = (self.heads, x[-1], self.channels)
+        self.attn_shape = (self.heads, self.channels)
         self.W_self = self.add_weight(name=f"kern_features_self",
-                                      shape=w_shape,
+                                      shape=self.w_shape,
                                       initializer=self.initializer,
                                       regularizer=self.regularizer,
                                       trainable=True
                                       )
         self.W_ngb = self.add_weight(name=f"kern_features_ngb",
-                                     shape=w_shape,
+                                     shape=self.w_shape,
                                      initializer=self.initializer,
                                      regularizer=self.regularizer,
                                      trainable=True
                                      )
         self.attn = self.add_weight(name=f"kern_attention",
-                                    shape=a_shape,
+                                    shape=self.attn_shape,
                                     initializer=self.initializer,
                                     regularizer=self.regularizer,
                                     trainable=True
@@ -55,14 +58,16 @@ class GATv2Layer(l.Layer):
     def call(self, inputs, training=None, mask=None):
         '''
         When the adjacency matrix is a Sparse Tensor batch size is not supported
-        :param inputs: Tuple of features NxF and Sparse Adjacency Matrix NxN
+        :param inputs: Tuple of features NxF and Sparse Adjacency Matrix NxN, in dense mode a tuple of BxNxF and Dense
+        Adjacency Matrix BxNxN
         :param training: Whether in training mode
         :param mask: Not Used
         :return: Updated Features of shape Nx(HF) or NF
         '''
         x, a = inputs
-        N = x.shape[-2]
         if isinstance(a, tf.sparse.SparseTensor):
+            # N = tf.reduce_max(a.indices) + 1
+            N = tf.shape(x, out_type=a.indices.dtype)[-2]
             i, j = a.indices[:, 0], a.indices[:, 1]
             x_i = tf.gather(x, i)
             x_i_prime = tf.einsum("EF,HFO->EHO", x_i, self.W_self)
@@ -72,26 +77,47 @@ class GATv2Layer(l.Layer):
             x_ij_prime = act.get(self.activation)(x_ij_prime)
             a_ij = tf.einsum("EHO,HO->EH", x_ij_prime, self.attn)
             a_soft = unsorted_segment_softmax(a_ij, j, N)
+            a_soft = self.dropout(a_soft)
             out = a_soft[..., None] * x_i[:, None]  # EH
             out = tf.math.unsorted_segment_sum(out, j, N)  # NHF
+            if self.concatenate_output:
+                out = tf.reshape(out, (-1, self.attn_shape[0] * self.attn_shape[1]))
+            else:
+                out = tf.math.reduce_mean(out, -2)
+            if self.return_attention:
+                return out, a_soft
+            else:
+                return out
         else:
-            raise NotImplemented(f"Adjacency Matrix must be a Sparse Tensor, but is {type(a)}")
-        if self.concatenate_output:
-            out = tf.reshape(out, (N, -1))
-        else:
-            out = tf.math.reduce_mean(out, -2)
-        if self.return_attention:
-            return out, a_soft
-        else:
-            return out
+            x_i = tf.einsum("BNF,HFO->BHON", x, self.W_self)
+            x_j = tf.einsum("BNF,HFO->BHON", x, self.W_ngb)
+            x_ij = x_i[..., None, :] + x_j[..., None]  # BHONN
+            x_ij_activated = act.get(self.activation)(x_ij)
+            e_ij = tf.einsum("BHONK,HO->BHNK", x_ij_activated, self.attn)
+            a_mask = tf.where(a == 0, -10e9, 0.0)
+            a_mask = tf.repeat(a_mask[:, None, ...], self.heads, 1)  # BHNN
+            a_ij = tf.nn.softmax(a_mask + e_ij)
+            a_ij = self.dropout(a_ij)
+            x_prime = tf.einsum("BHNK,BNF->BKHF", a_ij, x)
+            if self.concatenate_output:
+                out = tf.reshape(x_prime, (*x.shape[:2], self.heads * x.shape[-1]))  # BxNx(FH)
+            else:
+                out = tf.reduce_mean(x_prime, 2)  # BxNxF (reduce over heads)
+            if self.return_attention:
+                return out, a_ij
+            else:
+                return out
 
 
 class GATv2Model(m.Model):
-    def __init__(self, channels=[10,10,7], hidden_sizes=[10,10,7], heads=[10,10,1], activation="elu", *args, **kwargs):
+    def __init__(self, hidden_sizes=[10, 10, 7], channels=[10, 10, 7], heads=[10, 10, 1], dropout=0.2, activation="elu",
+                 *args,
+                 **kwargs):
         super(GATv2Model, self).__init__(**kwargs)
         self.channels = channels
         self.hidden_sizes = hidden_sizes
         self.heads = heads
+        self.dropout = l.Dropout(dropout)
         assert len(channels) == len(heads) == len(hidden_sizes)
         self.n_layers = len(hidden_sizes)
         self.activation = activation
@@ -103,12 +129,13 @@ class GATv2Model(m.Model):
         x, a = inputs
         for d, g in zip(self.dense_layers, self.gat_layers):
             x = d(x)
+            x = self.dropout(x)
             x = g([x, a])
         return x
 
 
 class GATModel(m.Model):
-    def __init__(self, channels, hidden_sizes, heads, activation="elu", *args, **kwargs):
+    def __init__(self, channels, hidden_sizes, heads, dropout, activation="elu", *args, **kwargs):
         super(GATModel, self).__init__(**kwargs)
         self.channels = channels
         self.hidden_sizes = hidden_sizes
@@ -116,7 +143,8 @@ class GATModel(m.Model):
         assert len(channels) == len(heads) == len(hidden_sizes)
         self.n_layers = len(hidden_sizes)
         self.activation = activation
-        self.gat_layers = [GATConv(attn_heads=hh, channels=c, activation=activation, dropout_rate=0, *args) for c, hh in
+        self.dropout = l.Dropout(dropout)
+        self.gat_layers = [GATConv(attn_heads=hh, channels=c, activation=activation, dropout_rate=dropout, *args) for c, hh in
                            zip(self.channels, self.heads)]
         self.dense_layers = [l.Dense(h, activation) for h in self.hidden_sizes]
 
@@ -124,6 +152,7 @@ class GATModel(m.Model):
         x, a = inputs
         for d, g in zip(self.dense_layers, self.gat_layers):
             x = d(x)
+            x = self.dropout(x)
             x = g([x, a])
         return x
 
@@ -131,17 +160,32 @@ class GATModel(m.Model):
 if __name__ == "__main__":
     from spektral import datasets
     import matplotlib.pyplot as plt
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sparse", action="store_true")
+    parser.add_argument("--epochs", type=int)
+    args = parser.parse_args()
+    sparse = args.sparse
+    epochs = args.epochs
 
     data = datasets.Citation("cora")
     data.download()
-    a = tf.sparse.from_dense(data.graphs[0].a.todense())
+    a = data.graphs[0].a.todense()
     x = data.graphs[0].x
     y = data.graphs[0].y
+    if sparse:
+        a = tf.sparse.from_dense(a)
+    else:
+        N = 500
+        a = a[None, :N, :N]
+        x = x[None, :N]
+        y = y[None, :N]
     i = l.Input(shape=(x.shape[-1]))
     o = l.Dense(10, "elu")(i)
     o = l.Dense(10, "elu")(o)
     o = l.Dense(7, "elu")(o)
-    dense_model = m.Model(i,o)
+    dense_model = m.Model(i, o)
 
     gat = GATModel(channels=[10, 10, 7], hidden_sizes=[15, 15, 7], heads=[10, 10, 1])
     gat2 = GATv2Model(channels=[10, 10, 7], hidden_sizes=[15, 15, 7], heads=[10, 10, 1])
@@ -166,8 +210,8 @@ if __name__ == "__main__":
         grads2 = tape.gradient(loss2, gat2.trainable_weights)
         optimizer2.apply_gradients(zip(grads2, gat2.trainable_weights))
         with tf.GradientTape() as tape:
-            out3 = dense_model(x)
-            loss3 = tf.keras.losses.categorical_crossentropy(y, out3, from_logits=True)
+            out3 = dense_model(x[0])
+            loss3 = tf.keras.losses.categorical_crossentropy(y[0], out3, from_logits=True)
             loss3 = tf.reduce_mean(loss3)
             loss3_hist.append(loss3)
         grads3 = tape.gradient(loss3, dense_model.trainable_weights)
@@ -178,3 +222,11 @@ if __name__ == "__main__":
     plt.plot(loss3_hist, label="Dense Loss")
     plt.legend()
     plt.show()
+
+    today = datetime.datetime.now().isoformat().replace(":", "_")
+    print(f"saving gatv2 to ./saved_models/gatv2_model_{today}")
+    gat2.save(f"./saved_models/gatv2_model_{today}")
+    print(f"saving gat to ./saved_models/gat_model_{today}")
+    gat.save(f"./saved_models/gat_model_{today}")
+    print(f"saving dense to ./saved_models/dense_model_{today}")
+    dense_model.save(f"./saved_models/dense_model_{today}")
